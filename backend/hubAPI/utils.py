9e05f0606c9e140aecb5a668714393a2ced3e492
Michael McCarthy <michemcc@outlook.com>
2025-09-19 16:44:41 -0400

    Cleanup repo

--- backend/hubAPI/utils.py
+++ backend/hubAPI/utils.py
@@ -1,18 +1,15 @@
 from typing import Union
 from django.http import JsonResponse
 
-
 def custom_server_error_response(error_message: str = '', status_code=500):
     if 500 <= status_code < 600:
         return JsonResponse({'success': False, 'message': error_message}, status=status_code)
     raise Exception # TODO: implement custom exceptions
 
-
 def custom_user_error_response(error_message: str = '', status_code=400):
     if 400 <= status_code < 500:
         return JsonResponse({'success': False, 'message': error_message}, status=status_code)
     raise Exception # TODO: implement custom exceptions
-
 
 def custom_success_response(success_message: Union[str, dict] = '', status_code=200):
     if 200 <= status_code < 300:
--- backend/hubModels/apps.py
+++ backend/hubModels/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class HubmodelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hubModels'
